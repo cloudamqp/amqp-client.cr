623bff5797571ee11e33cd6cfedd5d482df8465e
Carl Hörberg <carl@84codes.com>
2022-11-14 22:26:56 +0100

    move spec pending statments higher up
    they don't seem to apply otherwise

--- spec/amqp-client_spec.cr
+++ spec/amqp-client_spec.cr
@@ -356,15 +356,15 @@ describe AMQP::Client do
   end
 
   it "can connect over TLS" do
-    c = AMQP::Client.new(port: 5671, tls: true, verify_mode: OpenSSL::SSL::VerifyMode::NONE).connect
     pending! "CI doesn't support TLS"
+    c = AMQP::Client.new(port: 5671, tls: true, verify_mode: OpenSSL::SSL::VerifyMode::NONE).connect
     c.@io.class.should eq OpenSSL::SSL::Socket::Client
   end
 
   it "can reuse TLS context between multiple connections" do
+    pending! "CI doesn't support TLS"
     ctx = OpenSSL::SSL::Context::Client.insecure
     client = AMQP::Client.new(port: 5671, tls: ctx)
-    pending! "CI doesn't support TLS"
     conn1 = client.connect
     conn2 = client.connect
     conn1.@io.class.should eq OpenSSL::SSL::Socket::Client
